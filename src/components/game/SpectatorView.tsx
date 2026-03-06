6d1ed7d89f6ee2fd663feceffd8bc8b93b7bd455
davidchiradze <chiradze.dato00@gmail.com>
2026-03-06 23:09:35 +0400

    connection lost design

--- src/components/game/SpectatorView.tsx
+++ src/components/game/SpectatorView.tsx
@@ -111,7 +111,7 @@ function SpectatorVideoConference({
   const hasAnyTracks = tracks.length > 0;
 
   return (
-    <div className="w-full h-full flex items-center justify-center p-y-5 p-x-10">
+    <div className="w-full h-full flex items-center justify-center py-5 px-10">
       {!hasAnyTracks && <LoadingSpinner message="Waiting for players..." />}
       {hasAnyTracks && (
         <PlayerCircle
