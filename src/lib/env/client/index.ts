43d782005fcad8cbd07c3316a3f767270c6be567
davidchiradze <chiradze.dato00@gmail.com>
2026-06-04 12:23:43 +0400

    update env prefix with NEXT_PUBLIC_

--- src/lib/env/client/index.ts
+++ src/lib/env/client/index.ts
@@ -2,11 +2,11 @@
 import { z } from "zod";
 
 const clientEnvSchema = z.object({
-    ENVIRONMENT: z.string().min(1, "ENVIRONMENT is required"),
+    NEXT_PUBLIC_ENVIRONMENT: z.string().min(1, "ENVIRONMENT is required"),
 });
 
 const parsedClientEnv = clientEnvSchema.parse(process.env);
 
 export const clientEnv = {
-    environment: parsedClientEnv.ENVIRONMENT,
+    environment: parsedClientEnv.NEXT_PUBLIC_ENVIRONMENT,
 } as const;
