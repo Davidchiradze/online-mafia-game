343e9b56c699062f6a5ec8752edf51fa8adb2ae3
davidchiradze <chiradze.dato00@gmail.com>
2026-06-17 23:04:33 +0400

    remove profiles page

--- src/lib/constants/navigationSidebarItems.ts
+++ src/lib/constants/navigationSidebarItems.ts
@@ -15,8 +15,7 @@ export type NavigationSidebarItem = {
 
 export const NAVIGATION_SIDEBAR_ITEMS: NavigationSidebarItem[] = [
   { label: "Lobby", href: "/lobby", icon: Gamepad2 },
-  { label: "Community Chat", href: "/community-chat", icon: MessageSquare },
-  { label: "Profile", href: "/profile", icon: UserCircle },
   { label: "Match History", href: "/match-history", icon: History },
+  { label: "Community Chat", href: "/community-chat", icon: MessageSquare },
   { label: "Leaderboard", href: "/leaderboard", icon: Trophy },
 ];
