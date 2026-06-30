4bb26c4f956aec7799325e71ab980a46c00a077b
davidchiradze <chiradze.dato00@gmail.com>
2026-06-30 22:38:46 +0400

    detective visibility

--- src/components/participant/playerStates/index.ts
+++ src/components/participant/playerStates/index.ts
@@ -1,5 +1,6 @@
 export { default as DeadCover } from "./DeadCover";
 export { default as NightCover } from "./NightCover";
 export { default as DimmedCover } from "./DimmedCover";
+export { default as MaskedCover } from "./MaskedCover";
 export { default as DisconnectedCover } from "./DisconnectedCover";
 export { default as EmptySeat } from "./EmptySeat";
